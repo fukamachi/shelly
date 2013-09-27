b9d3548c8315b861f46bc038711614ce075fd6b2
Eitarow Fukamachi <e.arrows@gmail.com>
2013-09-27 12:16:12 +0900

    Replace hard tabs by spaces.

--- src/versions.lisp
+++ src/versions.lisp
@@ -14,26 +14,25 @@
   (dolist (package packages)
     (unless (find-package package)
       (let ((*standard-output* (make-broadcast-stream)))
-	(ql:quickload package :verbose nil)))))
+        (ql:quickload package :verbose nil)))))
 
 (defmacro i (symbol &optional (args nil p))
   (let* ((pos (position #\# (symbol-name symbol)))
-	 (intern `(intern ,(subseq (symbol-name symbol) (1+ pos))
-			  (find-package ,(subseq (symbol-name symbol) 0 pos)))))
+         (intern `(intern ,(subseq (symbol-name symbol) (1+ pos))
+                          (find-package ,(subseq (symbol-name symbol) 0 pos)))))
     (if p
-	`(funcall ,intern ,@args)
-	intern)))
+        `(funcall ,intern ,@args)
+        intern)))
 
 (let (releases)
   (defun retrieve-releases ()
     (qlrequire '(:yason :drakma :flexi-streams))
     (labels ((retrieve-from-api ()
                (let ((res
-		       (i #:yason#parse
-			   ((i #:flex#octets-to-string
-				((i #:drakma#http-request("https://api.github.com/repos/fukamachi/shelly/tags"))))))))
+                       (i #:yason#parse
+                          ((i #:flex#octets-to-string
+                              ((i #:drakma#http-request ("https://api.github.com/repos/fukamachi/shelly/tags"))))))))
                  res)))
-
       (or releases
           (setf releases (retrieve-from-api))))))
 
@@ -70,24 +69,24 @@
     (qlrequire '(:drakma :archive :chipz))
 
     (multiple-value-bind (tarball-stream status)
-        (i #:drakma#http-request(tarball-url :want-stream t))
+        (i #:drakma#http-request (tarball-url :want-stream t))
       (unless (= status 200)
         (error "Failed to download a tarball from GitHub (~A / status=~D)."
                tarball-url status))
 
       (unwind-protect
-          (extract-tarball tarball-stream destination)
+           (extract-tarball tarball-stream destination)
         (close tarball-stream)))))
 
 (defun extract-tarball (tarball-stream &optional (destination *default-pathname-defaults*))
   (let ((*default-pathname-defaults* destination))
     (qlrequire '(:archive :chipz))
     (let ((archive (i #:archive#open-archive
-		       ((i #:archive#tar-archive)
-			(i #:chipz#make-decompressing-stream((i #:chipz#gzip) tarball-stream))
-			:direction :input))))
+                      ((i #:archive#tar-archive)
+                       (i #:chipz#make-decompressing-stream ((i #:chipz#gzip) tarball-stream))
+                       :direction :input))))
       (prog1
-	  (merge-pathnames
-	   (i #:archive#name((i #:archive#read-entry-from-archive(archive))))
-	   *default-pathname-defaults*)
-        (i #:archive#extract-files-from-archive(archive))))))
+          (merge-pathnames
+           (i #:archive#name ((i #:archive#read-entry-from-archive (archive))))
+           *default-pathname-defaults*)
+        (i #:archive#extract-files-from-archive (archive))))))
